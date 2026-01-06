000aba6de3c103953d892c5c5d13f398e1cbc038
Tsingmian <19527837951@163.com>
2026-01-06 16:39:05 +0800

    init spring boot project

---
+++ src/main/java/com/example/movie_ticket_reservation/MovieTicketReservationApplication.java
@@ -0,0 +1,14 @@
+package com.example.movie_ticket_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketReservationApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/example/movie_ticket_reservation/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.movie_ticket_reservation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Movie Ticket System OK";
+    }
+}
